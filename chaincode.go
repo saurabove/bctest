0fc17fc66439eca9bb0e370ba78500680f8608c4
saurabh choudhary <saurabhchoudhary@saurabhs-MacBook-Pro-2.local>
2017-04-20 19:29:16 -0500

    change

--- chaincode.go
+++ chaincode.go
@@ -1251,27 +1251,7 @@ func (t *SimpleChaincode) updateAssetState(stub shim.ChaincodeStubInterface, arg
 	err := stub.PutState(crAsset.Id, jsonAsBytes)
 	fmt.Println("Success updated")
 	
-	//updating maximo
-
-	req, err := http.NewRequest("PUT","http://170.226.21.107/maxrest/rest/os/mxasset/2139?_action=change&description=Go Success1 Pipe bundle&_lid=maxadmin&_lpwd=maxadmin@GSCIND", nil)
-
-	// just printing the url that is formed 
-	//fmt.Println("http://170.226.21.107/maxrest/rest/os/mxasset?_lean=1&_lid=maxadmin&_lpwd=maxadmin@GSCIND&siteid=BEDFORD&assetnum="+myasset+"&description="+mydescp)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-    req.Header.Set("Content-Type", "text/plain")
-
-    client := &http.Client{}
-    res, err := client.Do(req)
-    if err != nil {
-        log.Fatal(err)
-    }
-    output, _ := ioutil.ReadAll(res.Body)
-    fmt.Println(string(output))  // printing the output response of the call
-    defer res.Body.Close()
-///maximo updated
+	
 
 	if err != nil {
 		fmt.Println("Error updating asset")
@@ -1399,6 +1379,28 @@ func (t *SimpleChaincode) updateAsset(stub shim.ChaincodeStubInterface,args []st
 	if err != nil {
 		return nil, err
 	}
+   //updating maximo
+
+	req, err := http.NewRequest("PUT","http://170.226.21.107/maxrest/rest/os/mxasset/2139?_action=change&description=Go Success1 Pipe bundle&_lid=maxadmin&_lpwd=maxadmin@GSCIND", nil)
+
+	// just printing the url that is formed 
+	//fmt.Println("http://170.226.21.107/maxrest/rest/os/mxasset?_lean=1&_lid=maxadmin&_lpwd=maxadmin@GSCIND&siteid=BEDFORD&assetnum="+myasset+"&description="+mydescp)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    req.Header.Set("Content-Type", "text/plain")
+
+    client := &http.Client{}
+    res, err := client.Do(req)
+    if err != nil {
+        log.Fatal(err)
+    }
+    output, _ := ioutil.ReadAll(res.Body)
+    fmt.Println(string(output))  // printing the output response of the call
+    defer res.Body.Close()
+///maximo updated
+   
 	return nil, nil
 }
 
