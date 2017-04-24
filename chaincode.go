b6dda894ceba76199c622b216318287fda65f3ce
saurabh choudhary <saurabhchoudhary@saurabhs-mbp-2.dfw.ibm.com>
2017-04-24 12:36:54 -0500

    change

--- chaincode.go
+++ chaincode.go
@@ -1381,7 +1381,7 @@ func (t *SimpleChaincode) updateAsset(stub shim.ChaincodeStubInterface,args []st
 		return nil, err
 	}
    //updating maximo************
-var peer_name string =os.Args[1:]
+var peer_name string =os.Args[1]
 
 if strings.Contains(peer_name, "vp0"){
 
